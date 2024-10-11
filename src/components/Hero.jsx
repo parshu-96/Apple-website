ea968f6567cb3da8323f77d73ecd215f12841023
parshu-96 <parshuvmahindrakar@gmail.com>
2024-10-11 19:07:56 +0530

    UI Update : Added iPhone Adv on screen
    Added Apple iPhone on UI and event listener to adjust the UI as per screen size

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,50 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { heroVideo, smallHeroVideo } from "../utils";
+import { useEffect, useState } from "react";
 function Hero() {
+  const [videoSrc, setVideoSrc] = useState(
+    window.innerWidth < 760 ? smallHeroVideo : heroVideo
+  );
+
+  const handleVideoScrSet = () => {
+    if (window.innerWidth < 760) {
+      setVideoSrc(smallHeroVideo);
+      _;
+    } else {
+      setVideoSrc(heroVideo);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleVideoScrSet);
+    return () => {
+      window.removeEventListener("resize", handleVideoScrSet);
+    };
+  }, []);
+  useGSAP(() => {
+    gsap.to("#hero", {
+      opacity: 1,
+      delay: 1,
+    });
+  }, []);
   return (
     <section className="w-full nav-height bg-black relative">
       <div className="h-5/6 w-full flex-center flex-col">
-        <p className="hero-title">iPhone 15 Pro</p>
+        <p id="hero" className="hero-title">
+          iPhone 15 Pro
+        </p>
+        <div className="mid:w-10/12 w-9/12">
+          <video
+            className="pointers-events-none"
+            autoPlay
+            muted
+            playsInline={true}
+            key={videoSrc}
+          >
+            <source src={videoSrc} type="video/mp4" />
+          </video>
+        </div>
       </div>
     </section>
   );
