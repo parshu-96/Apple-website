db710e51c7a9c3019edffcf4a3ef82a0f2d8bae0
parshu-96 <parshuvmahindrakar@gmail.com>
2024-10-13 00:18:55 +0530

    Completed Full UI with 3D iPhone
    Completed Full UI with 3D iPhone

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Navbar from "./components/Navbar";
 import Highlights from "./components/Highlights";
 import Hero from "./components/Hero";
+import Model from "./components/Model";
 const App = () => {
   return (
     <main className="bg-black">
       <Navbar />
       <Hero />
       <Highlights />
+      <Model />
     </main>
   );
 };
